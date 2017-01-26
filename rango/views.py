4160ded8222b7886ffcc011ea9e1226b28e7b5c1
Ben Stevenson <2227777s@student.gla.ac.uk>
2017-01-26 16:16:28 +0000

    End of Chapter 3

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import httpResponse
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Rango says hey there partner!")
