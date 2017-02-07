f848fc08a2e5e973108e9860edeb293ce33cdea9
Ben Stevenson <2227777s@student.gla.ac.uk>
2017-02-07 16:19:18 +0000

    Chapter 9.6 Done

--- rango/views.py
+++ rango/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rango.models import Category, Page
-from rango.forms import CategoryForm
-from rango.forms import PageForm
+from rango.models import Category, Page, UserProfile
+from rango.forms import CategoryForm, PageForm
 from django.core.urlresolvers import reverse
+from django import forms
+from rango.forms import UserForm, UserProfileForm
 
 def index(request):
 	category_list = Category.objects.order_by('-likes')[:5]
@@ -57,3 +58,34 @@ def add_page(request, category_name_slug):
 			print(form.errors)
 	context_dict = {'form':form, 'category':category}
 	return render(request, 'rango/add_page.html', context_dict)
+	
+	
+	
+def register(request):
+	registered = False
+	
+	if request.method == 'POST':
+		user_form = UserForm(data = request.Post)
+		profile_form = UserProfileForm(data=request.POST)
+		
+		if user_form.is_valid() and profile_form.is_valid():
+			user = user_form.save()
+			user.set_password(user.password)
+			user.save()
+			
+			profile = profile_form.save(commit = False)
+			profile.user = user
+			if 'picture' in request.FILES:
+				profile.picture = request.FILES['pictures']
+				profile.save()
+				registered = True
+		else:
+			print(user_form.erros, profile_form.errors)
+	else:
+		user_form = UserForm()
+		profile_form = UserProfileForm()
+	return render(request, 'rango/register.html',{ 'user_form':user_form, 'profile_form':profile_form,'registered':registered})
+		
+	
+	
+	
